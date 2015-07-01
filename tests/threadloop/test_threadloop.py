b9350ba4bc781be742e264883ab4d57316a7cb16
Grayson Koonce <grayson.koonce@gmail.com>
2015-06-30 17:56:00 -0700

    test that work is submitted concurrently

--- tests/threadloop/test_threadloop.py
+++ tests/threadloop/test_threadloop.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
+from decimal import Decimal
+import time
 
 import pytest
 
@@ -38,3 +40,48 @@ def test_coroutine_exception_propagates():
         with pytest.raises(TestException):
             future = threadloop.submit(coroutine)
             future.result()
+
+
+def test_submits_coroutines_concurrently():
+
+    @gen.coroutine
+    def coroutine1():
+        yield gen.sleep(.1)
+        raise gen.Return('coroutine1')
+
+    @gen.coroutine
+    def coroutine2():
+        yield gen.sleep(.1)
+        raise gen.Return('coroutine2')
+
+    @gen.coroutine
+    def coroutine3():
+        yield gen.sleep(.1)
+        raise gen.Return('coroutine3')
+
+    with ThreadLoop() as threadloop:
+
+        start = time.time()
+
+        future1 = threadloop.submit(coroutine1)
+        future2 = threadloop.submit(coroutine2)
+        future3 = threadloop.submit(coroutine3)
+
+        result1 = future1.result()
+        result2 = future2.result()
+        result3 = future3.result()
+
+        end = time.time() - start
+        took = round(Decimal(end), 2)
+
+        # should only take ~100 ks to finish both
+        # instead of ~300ms if they were executed serially
+        assert took == 0.1
+
+        assert result1 == 'coroutine1'
+        assert result2 == 'coroutine2'
+        assert result3 == 'coroutine3'
+
+
+def test_as_completed():
+    pass
