d55029fce202daa218fc7bbd4476bb219d9a5b31
Grayson Koonce <grayson.koonce@gmail.com>
2015-07-09 10:07:37 -0700

    bumping to 0.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='threadloop',
-    version='0.2.2',
+    version='0.3.0',
     author='Grayson Koonce',
     author_email='breerly@gmail.com',
     description='Tornado IOLoop Backed Concurrent Futures',
