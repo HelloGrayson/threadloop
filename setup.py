ccb595b0692b0950eaf1886ddd2e7e4acface211
Abhinav Gupta <abg@uber.com>
2015-07-13 14:31:48 -0700

    Back to development: 0.3.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='threadloop',
-    version='0.3.1',
+    version='0.3.2.dev0',
     author='Grayson Koonce',
     author_email='breerly@gmail.com',
     description='Tornado IOLoop Backed Concurrent Futures',
